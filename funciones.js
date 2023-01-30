ce9b67ccf1caffa7226370c217c9f8601af952b0
Marcos Daniel Gonzalez <marcosdanielgonzalez11@yahoo.com.ar>
2023-01-30 18:05:57 -0300

    problema de botton resuelto

--- funciones.js
+++ funciones.js
@@ -1,7 +1,28 @@
-onclick=function(){
-    switch(id){
-        case(info):
-        getelementid="datos";
-        break;
+/*$(document).ready(function(){
+    $(getElementById(info)).click(function(){
+        $(getElementById(datos)).fadeIoggle();
+    })
+});*/
+
+/*$(getElementById(info)).click(function() {
+    if ($(getElementById(datos)).css("display") == "none") {
+      $(getElementById(datos)).fadeIn();
+    } else {
+      $(getElementById(datos)).fadeOut();
     }
-}
+  });*/
+
+  /*function accion (){
+    
+        if (getElementById(datos).css("display") == "none") {
+            getElementById(datos).fadeIn();
+          } else {
+            getElementById(datos).fadeOut();
+          }
+    
+  }*/
+
+  document.getElementById('info').onclick = function(){
+    document.getElementById("datos").innerHTML = "mis datos"
+  };
+  
