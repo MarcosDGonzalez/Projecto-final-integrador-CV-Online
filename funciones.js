6bb010c6f902ceb6937ef47999859da5853535a7
Marcos Daniel Gonzalez <marcosdanielgonzalez11@yahoo.com.ar>
2023-01-31 16:39:09 -0300

    uso de json parcialmente exitoso (no podia llamar a un archivo json local)

--- funciones.js
+++ funciones.js
@@ -1,32 +1,15 @@
-/*$(document).ready(function(){
-    $(getElementById(info)).click(function(){
-        $(getElementById(datos)).fadeIoggle();
-    })
-});*/
 
-/*$(getElementById(info)).click(function() {
-    if ($(getElementById(datos)).css("display") == "none") {
-      $(getElementById(datos)).fadeIn();
-    } else {
-      $(getElementById(datos)).fadeOut();
-    }
-  });*/
-
-  /*function accion (){
-    
-        if (getElementById(datos).css("display") == "none") {
-            getElementById(datos).fadeIn();
-          } else {
-            getElementById(datos).fadeOut();
-          }
-    
-  }*/
+  //no me permitia referenciar a un archivo json local asi que opte por este metodo
+  const datosPersonales='{"cuil": "20-36069574-4", "fechaNacimiento": "19/07/1991", "nacionalidad": "Argentino", "estado": "soltero", "hijos": 0, "idiomas": "Español nativo, Ingles Bilingue", "conocimientos": "C#, Java, Python, SQL, OOP, Microsoft Office, Autocad, Mantenimiento y Reparacion de Systemas, Electronica","Particular": "(011)21264807"}';
+  const info=JSON.parse(datosPersonales);
+  const datosContacto='{"particular": "(011)21264807", "celular": "(+54)1133483172", "email": "marcosdanielgonzal ez11@yahoo.com.ar"}';
+  const contact=JSON.parse(datosContacto);
 
   document.getElementById("informacion").onclick = function(){
-    document.getElementById("datos").innerHTML = "Cuil:20-36069574-4 <br> Estado:Soltero <br> Hijos/as:0 <br> Idiomas:Español nativo, Ingles Bilingue <br> Conocimientos:C#, Java, Python, SQL, OOP, Microsoft Office, Autocad, Mantenimiento y Reparacion de Systemas, Electronica"
+    document.getElementById("datos").innerHTML = "Cuil:"+info.cuil+"<br> Fecha de Nacimiento:"+ info.fechaNacimiento+" <br> Estado:Soltero"+info.estado+" <br> Hijos/as:"+info.hijos+" <br> Idiomas:"+info.idiomas+" <br> Conocimientos:"+info.conocimientos
   };
   document.getElementById("contacto").onclick = function(){
-    document.getElementById("datos").innerHTML = "Particular: (011)21264807 <br> Celular: (+54)1133483172 <br> Email: marcosdanielgonzalez11@gmail.com <br> <a href="+"https://www.linkedin.com/in/marcos-daniel-gonzalez"+">Linkedin</a>"
+    document.getElementById("datos").innerHTML = "Particular: "+ contact.particular +"<br> Celular: "+ contact.celular +" <br> Email: "+ contact.email +" <br> <a href="+"https://www.linkedin.com/in/marcos-daniel-gonzalez"+">Linkedin</a>"
   };
   document.getElementById("educacion").onclick = function(){
     document.getElementById("datos").innerHTML = "Escuela de Educación Técnica N°1 <br> Secundaria: Completo <br> Título: Bachillerato <br> Titulo: Tècnico Electronico, pendiente <br> Desde el 2007 al 2010 <br><br> Universidad Nacional Arturo Jauretche <br> Curso de Desarrollo de Java OOP y SQL: Completo <br> Desde Feb 2019 a Dic 2019 <br><br> Universidad Nacional Arturo Jauretche <br> Ingeniería Informática:Incompleto Desde 2018 a 2021"
